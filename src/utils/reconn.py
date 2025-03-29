a484f7374f259a4d42f0637b865728a2350f10fe
Onur-TURAN <onurturan.t@gmail.com>
2025-03-29 22:51:15 +0300

    library

---
+++ src/utils/reconn.py
@@ -0,0 +1,10 @@
+import requests  # Web istekleri için
+import bs4       # Web scraping için (BeautifulSoup)
+import socket    # Ağ işlemleri için
+import scapy.all as scapy  # Ağ analizi ve paket işleme için
+import paramiko  # SSH bağlantıları için
+import nmap      # Ağ taraması için
+
+
+
+
